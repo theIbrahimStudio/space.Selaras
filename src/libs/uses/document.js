1e7267181561ffc82ed5f402f723e9d3c3b79937
100percentibrahim <ibrahimcpersonal@gmail.com>
2025-05-11 15:26:06 +0700

    chore: configure env

--- src/libs/uses/document.js
+++ src/libs/uses/document.js
@@ -1,7 +1,7 @@
 const useDocument = () => {
   const company = "Selaras Breathwork Studio";
   const short = "selaras-breathwork-studio";
-  const domain = import.meta.env.REACT_APP_DOMAIN_MAIN;
+  const domain = import.meta.env.REACT_APP_DOMAIN;
 
   return { company, short, domain };
 };
